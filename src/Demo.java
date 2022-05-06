3589737f7f28a6fd3bc2e2604dac161a27f385e1
unknown <veerjain1994@gmail.com>
2022-05-06 15:08:35 +0530

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my first java class");
+        System.out.println("I love my INDIA");
+
+
+      }
+}
