ed6058b3d78271456ecde5f922a4e136cc91dd37
riskywisky1994 <veerjain1994@gmail.com>
2022-05-06 22:18:38 +0530

    commit

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,6 @@ public class Demo {
     public static void main(String[] args) {
 
         System.out.println("This is my first java class");
-        System.out.println("I love my INDIA");
 
 
       }
